a0deae61d18e224f577a0327bf2b310673de22c2
nischaynidhi <58746151+nischaynidhi@users.noreply.github.com>
2022-02-17 20:37:36 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,33 +1,20 @@
-temp <- tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-power <- read.table(unz(temp,"household_power_consumption.txt"), 
-                    sep=";", 
-                    header = T, 
-                    na="?", 
-                    colClasses = c("character",
-                                   'character',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric'))
-
-unlink(temp)
-power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
-
-power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
-
-
-#plot4
-png(filename="plot4.png",width=480, height=480)
-par(mfrow=c(2,2))
-plot(x=power$POSIX ,y=power$Global_active_power, type = 'l', xlab='',ylab = 'Global Active Power')
-plot(x=power$POSIX ,y=power$Voltage, type = 'l', xlab='datetime',ylab = 'Voltage')
-plot(x=power$POSIX,y=power$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
-lines(x=power$POSIX,y=power$Sub_metering_2, col='red')
-lines(x=power$POSIX,y=power$Sub_metering_3, col='blue')
-legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1, bty = "n")
-plot(x=power$POSIX ,y=power$Global_reactive_power, type = 'l', xlab='datetime',ylab = 'Global_reactive_power')
+FileName <- "/Users/nischaynidhi/Desktop/coursera/household_power_consumption.txt"
+data_access <- read.table(FileName, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subData <- data_access[data_access$Date %in% c("1/2/2007","2/2/2007") ,]
+DateTime <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subData$Global_active_power)
+globalReactivePower <- as.numeric(subData$Global_reactive_power)
+Voltage <- as.numeric(subData$Voltage)
+SubMetering1 <- as.numeric(subData$Sub_metering_1)
+SubMetering2 <- as.numeric(subData$Sub_metering_2)
+SubMetering3 <- as.numeric(subData$Sub_metering_3)
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+plot(DateTime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(DateTime, Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(DateTime, SubMetering1, type="l", ylab="Energy sub metering", xlab="")
+lines(DateTime, SubMetering2, type="l", col="red")
+lines(DateTime, SubMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(DateTime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
