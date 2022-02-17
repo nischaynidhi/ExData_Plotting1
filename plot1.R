2f842a42adf8d08265442708dde0a6391f0baf4d
nischaynidhi <58746151+nischaynidhi@users.noreply.github.com>
2022-02-17 20:33:51 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,25 +1,7 @@
-temp <- tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-power <- read.table(unz(temp,"household_power_consumption.txt"), 
-                    sep=";", 
-                    header = T, 
-                    na="?", 
-                    colClasses = c("character",
-                                   'character',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric',
-                                   'numeric'))
-
-unlink(temp)
-power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
-
-power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
-
-#plot.1
-png(filename="plot1.png",width=480, height=480)
-hist(power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
+FileName <- "/Users/nischaynidhi/Desktop/coursera/household_power_consumption.txt"
+data_access <- read.table(FileName, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subData <- data_access[data_access$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
