6ba6070e7664476dc81b163ee87981125827b50a
nischaynidhi <58746151+nischaynidhi@users.noreply.github.com>
2022-02-19 00:20:59 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,6 @@
-FileName <- "/Users/nischaynidhi/Desktop/coursera/household_power_consumption.txt"
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 data_access <- read.table(FileName, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subData <- data_access[data_access$Date %in% c("1/2/2007","2/2/2007") ,]
 DateTime <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
